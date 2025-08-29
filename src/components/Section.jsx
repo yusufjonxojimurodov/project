ab46646c18e92ca9a35e6d8f1049631983b497c2
Yusuf Xojimurodov <xojimurodovyusuf7@gmail.com>
2025-08-29 15:25:45 +0500

    jonka test

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,10 +3,10 @@ import img from "../assets/img.svg"
 
 const Section = () => {
   return (
-    <div className='container flex justify-between items-center !mt-5'>
-        <div className='flex justify-center items-center flex-col gap-4'>
-            <h1>Управление запасами розничного магазина</h1>
-            <p>Использование сервиса 1С-Товары позволяет значительно упростить работу по формированию заявок поставщику.</p>
+    <div className='container flex justify-between items-center !mt-10'>
+        <div className='flex justify-center items-start flex-col gap-4'>
+            <h1 className='!font-semibold text-[60px]'>Управление запасами розничного магазина</h1>
+            <p className='w-[648px]'>Использование сервиса 1С-Товары позволяет значительно упростить работу по формированию заявок поставщику.</p>
             <button className='btn btn-primary w-[300px] h-[42px]'>Заказать демонстрацию</button>
         </div>
         <div>
