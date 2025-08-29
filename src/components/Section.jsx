010569a4e64b73122f6454adaa7626380d5fa0e6
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-08-29 15:15:23 +0300

    jonka

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import img from "img.svg"
 
 const Section = () => {
   return (
-    <div>Section</div>
+    <div className='container'>
+
+        <div>
+            <h1>Управление запасами розничного магазина</h1>
+            <p>Использование сервиса 1С-Товары позволяет значительно упростить работу по формированию заявок поставщику.</p>
+            <button className='btn w-[300px] h-[42px]'>Заказать демонстрацию</button>
+        </div>
+        <div>
+            <img src={img} alt="" />
+        </div>
+
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Section from './components/Section'
 
 createRoot(document.getElementById('root')).render(
   <>
-  
+  <Section/>
   </>,
 )
