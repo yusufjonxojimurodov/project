feff909d556ce2d45bcb075e9f47dff4c57cf321
Yusuf Xojimurodov <xojimurodovyusuf7@gmail.com>
2025-08-29 15:10:32 +0500

    project

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import HeaderComponent from "./components/HeaderComponent";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <>
-  
-  </>,
-)
+    <HeaderComponent />
+  </>
+);
